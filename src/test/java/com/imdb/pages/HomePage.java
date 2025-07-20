77e91a90321222bba76658d4eedbb772f2549b19
Kristine <kristin.ivanova20@gmail.com>
2025-07-20 20:42:21 +0300

    added file to .gitignore

--- src/test/java/com/imdb/pages/HomePage.java
+++ src/test/java/com/imdb/pages/HomePage.java
@@ -2,6 +2,7 @@ package com.imdb.pages;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import com.imdb.data.TestData;
 
 import static com.codeborne.selenide.Selenide.$;
 
@@ -9,7 +10,7 @@ public class HomePage {
     private final SelenideElement searchInput = $("input[placeholder*='Search'], input[name='q'], #suggestion-search");
 
     public void open() {
-        com.codeborne.selenide.Selenide.open("https://www.imdb.com");
+        com.codeborne.selenide.Selenide.open(TestData.BASE_URL);
     }
 
     public void search(String query) {
